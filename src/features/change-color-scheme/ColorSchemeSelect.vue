a90ad0e2277ce1d84e2b3fffd09c0dcb22c71315
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2023-05-03 18:41:15 +0600

    fix: change default select option

--- src/features/change-color-scheme/ColorSchemeSelect.vue
+++ src/features/change-color-scheme/ColorSchemeSelect.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue';
 
-const theme = ref('os');
+const theme = ref('system');
 
 watch(theme, () => {
   localStorage.setItem('theme', theme.value);
